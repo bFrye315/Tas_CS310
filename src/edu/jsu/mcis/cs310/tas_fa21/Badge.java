5b0e67858f0c191b60a53431a57009348c7798fe
James-Moll <lmollica2@gmail.com>
2021-10-06 21:33:01 -0500

    Filled out the badge class

--- src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -1,23 +1,33 @@
 
 package edu.jsu.mcis.cs310.tas_fa21;
 
-public class Badge {
+public class Badge {_
+        //Properties
     
     private String id;
     private String description;
     
-    public Badge(){
-        
+    //Constructer
+    public Badge(String badgeID, String badgeDescription){
+        this.id = badgeID;
+        this.description = badgeDescription;
     }
-        
+        //ID Getter
     public String getId() {
         return id;
     }
-
+        // ID setter
+    public void setID(String newID){
+            this.id = newID;
+}
+        //Description Getter
     public String getDescription() {
         return description;
     }
-    
+       //Desciption Setter
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
+    }
     @Override
     public String toString(){
         return "#" + id + " " + description;
