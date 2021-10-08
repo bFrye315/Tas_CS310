1699ca9aa319cdddba519a86d1d0905ae7aaf567
coutTylerNichols <tylernichols99@gmail.com>
2021-10-08 11:40:28 -0500

    shift commit

---
+++ src/edu/jsu/mcis/cs310/tas_fa21/Shift.java
@@ -0,0 +1,93 @@
+/*
+Problem Definition:
+Badge , Shift , and Punch: Implement a new Java class for each of these business objects. 
+Objects of the Badge class will contain information from the database about a single employee badge, 
+and objects of the Shift class will contain information from the database about a single shift ruleset 
+(that is, the starting time, stopping time, and other parameters of a single shift).
+Author: Tyler
+Date: 10/1/21
+ */
+package edu.jsu.mcis.cs310.tas_fa21;
+
+import java.util.*;
+import java.time.*;
+import java.io.*;
+import java.sql.*;
+import com.opencsv.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+public class Shift {
+     String description;
+     String start;
+     String stop;
+     String interval;
+     String graceperiod;
+     String dock;
+     String lunchstart;
+     String lunchstop;
+     String lunchdeduct;
+     int time;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStart() {
+        /*
+        “Shift Start” and “Shift Stop” to refer to the regularly scheduled starting 
+        and stopping times of the employee’s shift
+        */
+        
+        return start;
+    }
+
+    public String getStop() {
+        return stop;
+    }
+
+    public String getInterval() {
+        /*
+        The number of minutes beforethe startof a shift, and afterthe endof a shift, 
+        in which an employee's early "clock in" and late "clock out" punches are 
+        adjusted forwardto the scheduled start of their shift, or backwardto the 
+        end of their shift, respectively.
+        */
+        return interval;
+    }
+
+    public String getGraceperiod() {
+        /*
+        The number of minutes afterthe startof a shift, and beforethe endof a shift, 
+        in which an employee's late "clock in" punches or early "clock out" punches are "forgiven." 
+        */
+        return graceperiod;
+    }
+
+    public String getDock() {
+        /*
+        If a late "clock in" punch is made toolate after the start of the shift to 
+        fall within the grace period, the punch is adjusted forward in time from 
+        the start of the shiftby this amount (to discourage excess tardiness).
+        */
+        return dock;
+    }
+
+    public String getLunchstart() {
+        /*
+        “Lunch Start” and “Lunch Stop” to refer to the start and stop of the shift’s 
+        scheduled lunch break.
+        */
+        return lunchstart;
+    }
+
+    public String getLunchstop() {
+        return lunchstop;
+    }
+
+    public String getLunchdeduct() {
+        return lunchdeduct;
+    }
+     
+     
+}
