118b72d26eeb305022f660018797bb9ad08060d0
Brandon <bhf315@gmail.com>
2021-11-12 11:18:23 -0600

    cleaning up code

--- src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Badge.java
@@ -29,12 +29,8 @@ public class Badge {
         this.description = newDescription;
     }
     @Override
-    public String toString() {
-        
-        // #12565C60 (Chapman, Joshua E)
-        
-        StringBuilder s = new StringBuilder();
-        
+    public String toString() {// #12565C60 (Chapman, Joshua E)
+        StringBuilder s = new StringBuilder();        
         s.append('#').append(id).append(" (").append(description).append(')');
         
         return s.toString();
