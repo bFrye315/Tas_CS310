b69e3f17bf70766dbefb6d310d6fafc0f7ede5a4
Brandon <bhf315@gmail.com>
2021-11-12 11:17:48 -0600

    cleaning up code

--- src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -9,8 +9,6 @@ package edu.jsu.mcis.cs310.tas_fa21;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import org.json.simple.*; 
@@ -19,50 +17,10 @@ import org.json.simple.*;
 public class TAS {
 
     public static void main(String[] args) {
-        TASDatabase db;
-        db = new TASDatabase(); 
-       
-        //4943
-        //1087
-        //
-        Punch p = db.getPunch(3634);
-        Badge b = p.getBadge();
-        Shift s = db.getShift(b);
-        LocalDateTime ts = p.getOriginaltimestamp();
-        
-        
-        
-        ArrayList<Punch> dailypunchlist = db.getDailyPunchList(b, p.getOriginaltimestamp().toLocalDate());
-        
-        for (Punch punch : dailypunchlist) {
-            punch.adjust(s);
-        } 
-        calculateTotalMinutes(dailypunchlist,s);
-        
-        
-        
-        ArrayList<Punch> payperiod = db.getPayPeriodPunchList(b, p.getOriginaltimestamp().toLocalDate(), s);
-        StringBuilder sb = new StringBuilder();
-        for(Punch p1 : payperiod){
-            sb.append(p1.printAdjusted());
-            
-            
-        }
-        getPunchListAsJSON(payperiod);
-        System.out.println(sb.toString());
-        DecimalFormat df = new DecimalFormat("0.00");
-        System.out.println(df.format(calculateAbsenteeism(payperiod, s)));
-        double percentage = calculateAbsenteeism(payperiod, s);
-        Absenteeism a = db.getAbsenteeism(b, ts.toLocalDate());
-        System.out.println("badgeid: " + a.getBadgeid());
-        System.out.println("payperiod: " +a.getPayperiod());
-        System.out.println("percentage: " + a.getPercentage());
-        System.out.println(getPunchListPlusTotalsAsJSON(payperiod,  s));
+
     }
-    
-    
-    public static int calculateTotalMinutes(ArrayList<Punch> punchList, Shift shift) {
-        
+
+    public static int calculateTotalMinutes(ArrayList<Punch> punchList, Shift shift) {    
         int totalTime = 0;
         ArrayList<Punch> monPun = new ArrayList<>();
         ArrayList<Punch> tuePun = new ArrayList<>();
@@ -104,8 +62,7 @@ public class TAS {
                     sunPun.add(p);
                    
                     break;
-            }
-            
+            }            
         }
         if(satPun.size() > 0){
            listOfPunchByDay.add(satPun); 
@@ -130,16 +87,12 @@ public class TAS {
         }
 
         for(int i = 0; i < listOfPunchByDay.size(); i++){
-            System.out.println("size: " + listOfPunchByDay.size());
-            System.out.println("size of inner array: " + listOfPunchByDay.get(i).size());
             ArrayList<Punch> day = listOfPunchByDay.get(i);
             try {
-
                 for (int j = 0; j < day.size(); j+=2) {
                     Duration duration = Duration.between(day.get(j).getAdjustedtimestamp(), day.get(j + 1).getAdjustedtimestamp());
                     int totalMinutes = (int)duration.toMinutes();
                     totalTime = totalTime + totalMinutes;
-
                 }
                 boolean lunchClockOut = false;
                 for(Punch punch : day){
@@ -150,9 +103,7 @@ public class TAS {
                 }
                 if(!lunchClockOut && totalTime >= shift.getLunchdeduct()){
                     totalTime = totalTime - shift.getLunchduration();
-                }
-                System.out.println(totalTime);
-
+                }               
             }
             catch(IndexOutOfBoundsException e1) {
                 System.out.println("Odd number of punches");
@@ -160,26 +111,14 @@ public class TAS {
             catch(Exception e) {
                 e.printStackTrace();
             }
-        }   
-            
-            
-        
-        System.out.println(totalTime);
+        }           
         return totalTime;
-
     }
     
     //Feature 5
     public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
         DateTimeFormatter format = DateTimeFormatter.ofPattern("EEE" + " LL/dd/uuuu HH:mm:ss");
         ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
-        //"[{\"originaltimestamp\":\"TUE 09\\/18\\/2018 11:59:33\",
-        //\"badgeid\":\"08D01475\",
-        //\"adjustedtimestamp\":\"TUE 09\\/18\\/2018 12:00:00\",
-        //\"adjustmenttype\":\"Shift Start\",
-        //\"terminalid\":\"104\",
-        //\"id\":\"4943\",
-        //\"punchtype\":\"CLOCK IN\"}
         for(Punch punch: dailypunchlist){
             
             HashMap<String, String> punchData = new HashMap<>();
@@ -191,13 +130,9 @@ public class TAS {
             punchData.put("adjustmenttype", String.valueOf(punch.getAdjustmenttype()));
             punchData.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp().format(format).toUpperCase()));
             punchData.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp().format(format).toUpperCase()));
-            jsonData.add(punchData);
-            
-        }
-        
+            jsonData.add(punchData);            
+        }        
         String json = JSONValue.toJSONString(jsonData);
-
-        System.out.println(json);
         return json;
     }
     
@@ -207,8 +142,7 @@ public class TAS {
         double totalWeeklyMinutes = 0;
         final double FORTYHRWEEK = 2400;
         final double TOP_PERCENTAGE = 100;
-        
-      
+
         totalWeeklyMinutes = calculateTotalMinutes(punchlist, s);
         
         percentage = (totalWeeklyMinutes/FORTYHRWEEK) * TOP_PERCENTAGE;
@@ -218,18 +152,14 @@ public class TAS {
         
         return percentage;
     }
-    
-    
-    
+
     public static String getPunchListPlusTotalsAsJSON(ArrayList<Punch> punchlist, Shift s){
         DecimalFormat df = new DecimalFormat("0.00");
         DateTimeFormatter format = DateTimeFormatter.ofPattern("uuuu-LL-dd HH:mm:ss");
-        //ArrayList<HashMap<String, String>> punches = new ArrayList<>();
         ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
         JSONObject timePercentage = new JSONObject();
         
-        for(Punch punch: punchlist){
-            
+        for(Punch punch: punchlist){            
             JSONObject punchData = new JSONObject();
             
             punchData.put("id", String.valueOf(punch.getId()));
@@ -239,15 +169,12 @@ public class TAS {
             punchData.put("adjustmenttype", String.valueOf(punch.getAdjustmenttype()));
             punchData.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp().format(format).toUpperCase()));
             punchData.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp().format(format).toUpperCase()));
-            jsonData.add(punchData);
-            
+            jsonData.add(punchData);     
         }
-
         timePercentage.put("totalminutes", String.valueOf(calculateTotalMinutes(punchlist, s)));
         timePercentage.put("absenteeism", String.valueOf(df.format(calculateAbsenteeism(punchlist, s)) + "%"));
         timePercentage.put("punchlist", jsonData);
-        
-        
+
         String json = JSONValue.toJSONString(timePercentage);
         return json;
     }
